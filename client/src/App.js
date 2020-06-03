b72852ddf71a4be5f4eb24688af4de4927d45ee6
James Dobbe <dobbe2@gmail.com>
2020-06-02 23:11:08 -0700

    changes to push to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header.js"
-import Body from "./components/Body/Body.js";
 import BookSearch from "./components/BookSearch/BookSearch.js"
 
 class App extends Component {
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -6,8 +6,6 @@ const Header = () => {
     return(
         <header>
         <img id="header_image" src={TitleImage} />
-        {/* <h1>Google Books Search</h1>
-        <i className="fas fa-book fa-3x"></i> */}
         </header>
     )
 }
